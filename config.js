79bfe7af0ea458d0acc4b7be570a2899c7c2abc6
maxxin3 <109416723+maxxin3@users.noreply.github.com>
2022-07-23 12:42:56 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+   
+    "token": "",
+    "prefix": ".",
+    "mongooseConnectionString": "",
+    "embedcolor": "#118fff",
+    "guildid": "",
+    "developers": [""],
+    "OWNER_ID": "",
+};
---
+++ index.js
@@ -0,0 +1,47 @@
+//PACK
+const { Client, Collection, GatewayIntentBits, Partials } = require("discord.js");
+
+const client = new Client({
+  disableEveryone: true,
+  shards: "auto",
+  disableMentions: "everyone",
+  partials: [Partials.Channel, Partials.Message, Partials.User, Partials.GuildMember, Partials.Reaction],
+  intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildIntegrations,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildWebhooks,
+        GatewayIntentBits.GuildInvites,
+        GatewayIntentBits.GuildVoiceStates,
+        GatewayIntentBits.GuildPresences,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMessageReactions,
+        GatewayIntentBits.GuildMessageTyping,
+        GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.DirectMessageReactions,
+        GatewayIntentBits.DirectMessageTyping,
+
+      ],
+});
+
+module.exports = client;
+
+// Global Variables
+client.commands = new Collection();
+
+client.config = require("./config.js");
+
+const mongoose = require("mongoose");
+const prefix = client.config.prefix
+const token = client.config.token
+
+// Initializing the project
+require("./handler")(client);
+  
+
+client.on('ready', async () => {
+  console.log(`${client.user.tag} is now Onlone! Prefix: ${client.config.prefix}`.bgGreen);
+});
+
+client.login(client.config.token);
