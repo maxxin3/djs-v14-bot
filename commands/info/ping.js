7a3534c4a7cd93f488c198c42973bd6640ee4e40
maxxin3 <109416723+maxxin3@users.noreply.github.com>
2022-07-23 12:43:56 +0300

    Add files via upload

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -1,1 +1,20 @@
+const { EmbedBuilder } = require('discord.js');
 
+module.exports = {
+    name: "ping",
+    description: "Check Bot's Ping",
+    aliases: ["ping"],
+    run: async (client, message, args) => {
+    ////
+
+await message.reply({ content: "**Getting The Latency**" }).then(async (msg) => {
+      var ping = msg.createdAt - message.createdAt;
+      var api_ping = client.ws.ping;
+
+      const PingEmbed = new EmbedBuilder()
+        .setColor("Blue")
+        .setDescription(`\`\`\`ini\n[ Bot Latency ] :: ${ping}ms \n[ API Latency ] :: ${api_ping}ms \`\`\``)
+      await msg.edit({ content: "** **", embeds: [PingEmbed] })
+    })
+  }
+}
