2400744bc039934bfb73272b0bc4e54980e5d2d6
maxxin3 <109416723+maxxin3@users.noreply.github.com>
2022-07-23 13:54:26 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
    
-    "token": "", required, https://discordjs.guide/preparations/setting-up-a-bot-application.html#your-token
+    "token": "", // required, https://discordjs.guide/preparations/setting-up-a-bot-application.html#your-token
     "prefix": ".", // required, command prefix
     "mongooseConnectionString": "", optional.
     "embedcolor": "#118fff",
-    "guildid": "", required, after new update for slash-commands 
-    "OWNER_ID": "", required, for owner-only commands
+    "guildid": "", // required, after new update for slash-commands 
+    "OWNER_ID": "", // required, for owner-only commands
 };
