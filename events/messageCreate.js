e67e07b071b0732cbe383f44e53c758963a17ca6
maxxin3 <109416723+maxxin3@users.noreply.github.com>
2022-08-05 00:06:26 +0300

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,3 +1,4 @@
+const { EmbedBuilder, Message, Client, PermissionsBitField } = require("discord.js");
 const { Collection } = require('discord.js');
 const Timeout = new Collection();
 const ms = require('ms')
@@ -23,6 +24,21 @@ client.on("messageCreate", async (message) => {
      if(command.ownerOnly) {
      if(message.author.id !== client.config.OWNER_ID) return client.error("Bot owner can only use this command", message)
     }
+
+    if (command.botPerms) {
+            if (!message.guild.members.me.permissions.has(PermissionsBitField.resolve(command.botPerms || []))) {
+                embed.setDescription(`I don't have **\`${command.botPerms}\`** permission in <#${message.channelId}> to execute this **\`${command.name}\`** command.`);
+                return message.channel.send({ embeds: [embed] });
+            }
+        }
+
+       if (command.userPerms) {
+            if (!message.member.permissions.has(PermissionsBitField.resolve(command.userPerms || []))) {
+                embed.setDescription(`You don't have **\`${command.userPerms}\`** permission in <#${message.channelId}> to execute this **\`${command.name}\`** command.`);
+                return message.channel.send({ embeds: [embed] });
+            }
+        }
+
 
   await command.run(client, message, args);
     
